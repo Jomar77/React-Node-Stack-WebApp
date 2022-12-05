dcd4c0a6931210a87f027d416d66c038323c4a3e
JOMAR NACORDA <jnacorda04@gmail.com>
2022-12-05 20:55:34 +1300

    changed somethjing

--- api/index.js
+++ api/index.js
@@ -4,13 +4,9 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/test", (req, res) => {
+  res.json("Hello World!")
+})
 app.listen(8800, () => {
   console.log("Connected!");
-});     
-    
-
-
-
-
-
-
+})
