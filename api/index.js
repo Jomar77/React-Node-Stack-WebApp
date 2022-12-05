6196233ee3e1bd09014a40c7472f176e6a69c011
JOMAR NACORDA <jnacorda04@gmail.com>
2022-12-05 21:06:16 +1300

    added use in index api

--- api/index.js
+++ api/index.js
@@ -1,8 +1,16 @@
 import express from "express"
+import authRoutes from "./routes/auth.js"
+import postRoutes from "./routes/posts.js"
+import userRoutes from "./routes/users.js"
+
 
 const app = express();
 
 app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/users", userRoutes);
+
 
 app.get("/test", (req, res) => {
   res.json("Hello World!")
